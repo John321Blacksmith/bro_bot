1b1496d1ad7c7466bc0959ed085070cd70437834
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-02-18 13:17:19 +0300

    deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from telebot.credentials import API_TOKEN
-
-
-message = 'Hello , World'
-
-url = f"https://api.telegram.org/bot{API_TOKEN}/sendMessage?chat_id=1418255178&text={message}"
-
-print(requests.get(url).json()['ok'])
